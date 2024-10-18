1ccad5068f760cb2fcb1e3682d44a279c2ba388f
Yaroslav Ahulov <94825812+Agulov-Yaroslav@users.noreply.github.com>
2024-10-18 09:00:40 +0200

    second commit

--- main.py
+++ main.py
@@ -4,25 +4,21 @@ import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-import random
-
+#нужно добавить стемминг или Лемминг!!!
+#и изменить поиск сделать так чтобы поиск осуществлялся в массиве r'\bдесн(а|ы|е|у|ной)\b'
 
 API_TOKEN = '7853274476:AAEZk8nbMTaPCbsXPl4tXxgiEGPbvFogFPw'
 
 bot = telebot.TeleBot(API_TOKEN)
- # Вместо Lemm
 stop_words = set(stopwords.words('russian')) # Загружаем русские стоп-слова
 
 def preprocess_input(user_input):
-    tokens = word_tokenize(user_input)
+    tokens = word_tokenize(user_input, language='russian')
     processed_words = []
     for word in tokens:
         # Если это слово состоит только из букв/цифр и не является стоп-словом
         if word.isalnum() and word not in stop_words:
-            # Приводим слово к его базовой форме (лемматизация) и добавляем в список
-            lemmword = morph.parse(word)[0].normal_form
-            print(lemmword)
-            processed_words.append(lemmword)
+            processed_words.append(word)
 
     return processed_words
 
@@ -38,10 +34,10 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)  # Обработка всех сообщений
 def check_message(message):
     mess_low = message.text.lower()
-    afer_proc_words = preprocess_input(mess_low)
-    print(afer_proc_words)
+    processed_words = preprocess_input(mess_low)
+    print(processed_words)
 
-    keyword_pattern = r'\bдесна\b|\bдесны\b|\bдесне\b|\bдесну\b|\bдесной\b|\bдесной\b'
+    keyword_pattern = r'\bдесн(а|ы|е|у|ной)\b'
     if re.search(keyword_pattern, message.text.lower()):
         bot.send_message(message.chat.id, 'Я тут')
 
